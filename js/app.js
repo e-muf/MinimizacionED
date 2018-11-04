4ae31fb3fd76a7c24902b0dc7af8f46aae957375
Emanuel Flores Martínez <mufymes@gmail.com>
2018-11-03 22:55:34 -0600

    Agregando Preguntas

--- js/app.js
+++ js/app.js
@@ -55,16 +55,16 @@
 
 		}
 
-		const pregunta1 = new Pregunta('¿Como te llamas?', 'X', 'Y', 'G', 'Z');
-		const pregunta2 = new Pregunta('¿Como estas?', 'X', 'Y', 'G', 'Z');
-		const pregunta3 = new Pregunta('¿Cual es tu libro favorito?', 'X', 'Y', 'G', 'Z');
-		const pregunta4 = new Pregunta('¿Cual es tu comida favorita?', 'X', 'Y', 'G', 'Z');
-		const pregunta5 = new Pregunta('¿Qué música te gusta?', 'No se', 'Y', 'G', 'Z');
-		const pregunta6 = new Pregunta('¿Cuáles son los métodos de minimización?', 'X', 'Y', 'G', 'Z');
-		const pregunta7 = new Pregunta('¿Cuál método es mejor?', 'X', 'Y', 'G', 'Z');
-		const pregunta8 = new Pregunta('¿Qué es un estadístico?', 'X', 'Y', 'G', 'Z');
-		const pregunta9 = new Pregunta('¿Porque llevo estructras discretas?', 'X', 'Y', 'G', 'Z');
-		const pregunta10 = new Pregunta('¿Que significa esto?<img class="img-pregunta" src="img/biblio.jpg">', 'X', 'Y', 'G', 'Z');
+		const pregunta1 = new Pregunta('¿Cuando conviene realizar un proceso de minimización?', 'Cuando una red se va a producir de manera masiva.', 'Si tenemos una red lógica que usaremos una vez.', 'Cuando el tiempo en el que se lleva la minimización es corto.', 'Cuando el tiempo de minimización no importa.');
+		const pregunta2 = new Pregunta('En Algebra de Boole, ¿Qué significa el operador "-"?', 'NOT', 'AND', 'XOR', 'OR');
+		const pregunta3 = new Pregunta('¿Cuales son los operadores con los que se representa AND y OR?', '* y +', '+ y *', '- y +', '+ y -');
+		const pregunta4 = new Pregunta('¿Cual método se presentó en 1953?', 'Mapa Karnaugh', 'Método de Quine-McCluskey', 'Método Algebraico', 'E.W. Veitch');
+		const pregunta5 = new Pregunta('Dos cuadrados de un mapa de Karnaugh son adyacentes cuando...', 'los minterms que representan difieren únicamente en un literal', 'los minterms que representan difieren en más de un literal', 'los minterms que representan no difieren', 'los minterms que representan estan juntos');
+		const pregunta6 = new Pregunta('Selecciona el método de minimización', 'Mapa de Karnaugh', 'Óxido-Reducción', 'Lema de Arden', 'Suma canónica de productos');
+		const pregunta7 = new Pregunta('La tabla de verdad de A ^ B es: ', 'V<br>F<br>F<br>F', 'V<br>V<br>V<br>F', 'V<br>F<br>V<br>V', 'V<br>F<br>F<br>V');
+		const pregunta8 = new Pregunta('¿Cuando dos expresiones booleanas son equivalentes?', 'Si tiene las mismas funciones de verdad', 'Si son exactamente iguales', 'Si son parecidas en al menos un término', 'Si son parecidas en todos los terminos.');
+		const pregunta9 = new Pregunta('¿Qué pasa cuando en un mapa de Karnaugh dos cuadrados adyacentes contienen un 1?', 'Se pueden combinar en un producto que contendrá únicamente las variables o literal común', 'Se separan las variables que los contienen', 'Se juntan las variables con valor diferente', 'Se juntan las variables las cuales son iguales');
+		const pregunta10 = new Pregunta('Un bloque de 4 cuadrados elimina: ', '0 variables', '1 variable', '2 variables', '4 variables');
 
 		var t_preguntas = [
 		pregunta1,
