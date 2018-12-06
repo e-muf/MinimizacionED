0beedd4102dd044777ea65e9939a712f2445fef0
Emanuel Flores <mufymes@gmail.com>
2018-12-06 15:47:07 -0600

    ErroresMenoresResueltos

--- js/app.js
+++ js/app.js
@@ -55,14 +55,14 @@
 
 		}
 
-		const pregunta1 = new Pregunta('¿Cuando conviene realizar un proceso de minimización?', 'Cuando una red se va a producir de manera masiva.', 'Si tenemos una red lógica que usaremos una vez.', 'Cuando el tiempo en el que se lleva la minimización es corto.', 'Cuando el tiempo de minimización no importa.');
+		const pregunta1 = new Pregunta('¿Cuándo conviene realizar un proceso de minimización?', 'Cuando una red se va a producir de manera masiva.', 'Si tenemos una red lógica que usaremos una vez.', 'Cuando el tiempo en el que se lleva la minimización es corto.', 'Cuando el tiempo de minimización no importa.');
 		const pregunta2 = new Pregunta('En Algebra de Boole, ¿Qué significa el operador "-"?', 'NOT', 'AND', 'XOR', 'OR');
-		const pregunta3 = new Pregunta('¿Cuales son los operadores con los que se representa AND y OR?', '* y +', '+ y *', '- y +', '+ y -');
-		const pregunta4 = new Pregunta('¿Cual método se presentó en 1953?', 'Mapa Karnaugh', 'Método de Quine-McCluskey', 'Método Algebraico', 'E.W. Veitch');
+		const pregunta3 = new Pregunta('¿Cuáles son los operadores con los que se representa AND y OR?', '* y +', '+ y *', '- y +', '+ y -');
+		const pregunta4 = new Pregunta('¿Cuál método se presentó en 1953?', 'Mapa Karnaugh', 'Método de Quine-McCluskey', 'Método Algebraico', 'E.W. Veitch');
 		const pregunta5 = new Pregunta('Dos cuadrados de un mapa de Karnaugh son adyacentes cuando...', 'los minterms que representan difieren únicamente en un literal', 'los minterms que representan difieren en más de un literal', 'los minterms que representan no difieren', 'los minterms que representan estan juntos');
 		const pregunta6 = new Pregunta('Selecciona el método de minimización', 'Mapa de Karnaugh', 'Óxido-Reducción', 'Lema de Arden', 'Suma canónica de productos');
 		const pregunta7 = new Pregunta('La tabla de verdad de A ^ B es: ', 'V<br>F<br>F<br>F', 'V<br>V<br>V<br>F', 'V<br>F<br>V<br>V', 'V<br>F<br>F<br>V');
-		const pregunta8 = new Pregunta('¿Cuando dos expresiones booleanas son equivalentes?', 'Si tiene las mismas funciones de verdad', 'Si son exactamente iguales', 'Si son parecidas en al menos un término', 'Si son parecidas en todos los terminos.');
+		const pregunta8 = new Pregunta('¿Cuándo dos expresiones booleanas son equivalentes?', 'Si tiene las mismas funciones de verdad', 'Si son exactamente iguales', 'Si son parecidas en al menos un término', 'Si son parecidas en todos los terminos.');
 		const pregunta9 = new Pregunta('¿Qué pasa cuando en un mapa de Karnaugh dos cuadrados adyacentes contienen un 1?', 'Se pueden combinar en un producto que contendrá únicamente las variables o literal común', 'Se separan las variables que los contienen', 'Se juntan las variables con valor diferente', 'Se juntan las variables las cuales son iguales');
 		const pregunta10 = new Pregunta('Un bloque de 4 cuadrados elimina: ', '0 variables', '1 variable', '2 variables', '4 variables');
 		const pregunta11 = new Pregunta('¿Cuál corresponde a la ley de Dominación?', 'p ^ (F) <=> (F)<br>p v (T) <=> (T)', 'p v (F) <=> (F)<br>p ^ (T) <=> (T)', 'p ^ (F) <=> (T)<br>p v (T) <=> (F)', 'p v (T) <=> (F)<br>p ^ (F) <=> (T)');
@@ -122,7 +122,7 @@
 				$("#incompletas").modal();
 			}
 			else {
-				$(".modal-body").html('<p>Tu califacación es:&nbsp <span class="calificacion font-weight-bold">' + calificacion + '</span></p>');
+				$(".modal-body").html('<p>Tu calificación es:&nbsp <span class="calificacion font-weight-bold">' + calificacion + '</span></p>');
 				$('#completas').modal();
 			}
 		}
